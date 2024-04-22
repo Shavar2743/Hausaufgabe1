ccdd8d84284f56fbb1eedd1393ef63b5625cb676
Shavar2743 <markus.shavar@web.de>
2024-04-22 22:33:33 +0200

    Taschenrechner initialisiert

---
+++ home1.py
@@ -0,0 +1,18 @@
+operatorliste = ["+", "-", "/", "*"]
+
+zahl1 = int(input("Erste Zahl: "))
+operator = input("Operator: ")
+zahl2 = int(input("Zweite Zahl: "))
+
+if operator in operatorliste:
+    if operator == "+":
+        ergebnis = zahl1 + zahl2
+    elif operator == "-":
+        ergebnis = zahl1 - zahl2
+    elif operator == "/":
+        ergebnis = zahl1 / zahl2
+    elif operator == "*":
+        ergebnis = zahl1 * zahl2
+    print(ergebnis)
+else:
+    print("Gib einen gültigen Operator ein")
